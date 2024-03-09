0191d1700ba103cb2b7980825a084f361a793492
Rodolfo Escaleira <74121364+eXclus1on@users.noreply.github.com>
2024-03-09 10:35:23 +0000

    Update search term placeholder and variable name.

--- p4-forms/index.php
+++ p4-forms/index.php
@@ -1,15 +1,25 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Aula 09-03-2024</title>
 </head>
+
 <body>
+
+    <?php
+    $searchTerm  = isset($_GET['searchTerm']) ? $_GET['searchTerm'] : 'pokemon';
+    ?>
+    <h1>Search Term: <?php echo $searchTerm ?></h1>
     <form action="index.php" method="get">
-        <input type="text" placeholder="search" name="S">
+        <input type="text" placeholder="search" name="searchTerm">
         <button type="submit">Search</button>
     </form>
-    
+
+
+
 </body>
+
 </html>
