0374e522c4ea94bc4ee9df33d04aa88c2cc26358
Rodolfo Escaleira <74121364+eXclus1on@users.noreply.github.com>
2024-03-09 10:16:44 +0000

    file-p4-create-with-index.php

---
+++ p4-forms/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aula 09-03-2024</title>
+</head>
+<body>
+    <forms action="">
+        <input type="text" placeholder="search">
+        <button type="submit">Search</button>
+    </forms>
+    
+</body>
+</html>
